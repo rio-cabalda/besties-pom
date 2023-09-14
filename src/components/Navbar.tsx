852b85468696c5af8301e5a655137cc4ef493d4d
rio-cabalda <riojoycabalda14@gmail.com>
2023-09-14 22:18:34 +0800

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,41 +4,41 @@ import { Link, useLocation } from 'react-router-dom';
 import { BsBag } from 'react-icons/bs';
 import { navLinks } from '../utils/constant';
 import logo from '../assets/logo.png';
-import Sidebar from './Sidebar';
+import {Sidebar} from '.';
 import { useProductStore } from '../store/productStore';
 import useCurrentPath from '../hooks/useCurrentPath';
-
 
 const Navbar = () => {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
   const [navLink, setNavLink] = useState<string>('');
   const navRef = useRef<HTMLHeadingElement | null>(null);
-  const { setNavHeight } = useProductStore();
+  const { navHeight,setNavHeight } = useProductStore();
   const {pathname} = useLocation();
   const path = useCurrentPath(pathname.toString());
 
   useEffect(() => {
     
     if (navRef.current) {
-      const height = navRef.current.clientHeight;
-      setNavHeight(height.toLocaleString());
+      const height = navRef.current.offsetHeight;
+      setNavHeight(height);    
     }
     setNavLink(path);
+
     // eslint-disable-next-line
-  }, [navRef, pathname]);
+  }, [navHeight, pathname]);
 
   return (
     // <nav className='max-w-screen-xl mx-auto flex justify-between items-center p-4 xl:px-0'>
-    <header ref={navRef} className='w-full bg-slate-300 fixed top-0 left-0 z-30'>
-      <nav className='max-w-screen-xl mx-auto p-4 flex justify-between items-center xl:px-0'>
+    <header ref={navRef} className='w-full bg-white shadow-sm fixed top-0 left-0 z-30'>
+      <nav className='max-w-screen-xl mx-auto py-2 px-4 flex justify-between items-center xl:px-0'>
         {/* left side */}
-          <div className='flex items-center gap-2 mr-4'>
+          <div className='flex items-center gap-2'>
             <button onClick={()=>setSidebarOpen(!sidebarOpen)} className='cursor-pointer mr-2 transition-transform transform-gpu transform-origin-center hover:animate-pulsing'>
                 <AiOutlineMenu size={30} />
             </button>
-            <Link to='/' className='flex items-center'>
-              <img src={logo} alt="Logo" className="h-14 " />
-              <h1 className='hidden sm:block text-2xl transform lg:text-3xl px-2 leading-tight'>
+            <Link to='/' className='flex items-center justify-center'>
+              <img src={logo} alt="Logo" className="h-16 w-16" />
+              <h1 className='hidden sm:block text-3xl transform lg:text-4xl px-4 leading-tight'>
                 Besties
                 <span className='font-bold'> Pom</span>
               </h1>
@@ -47,13 +47,13 @@ const Navbar = () => {
      
 
         {/* search input */}
-        <div className='bg-gray-200 px-2 md:ml-6 rounded-full flex items-center  flex-1  max-w-lg '>
+        <div className='bg-gray-200 px-2 md:mx-6 rounded-full flex items-center flex-1  max-w-lg '>
           <AiOutlineSearch size={25} />
           <input className='bg-transparent p-2 w-full focus:outline-none' type="text" name="search" id="search" placeholder='search product' />
         </div>
 
         {/* Links */}
-        <ul className='hidden md:flex md:px-6 capitalize justify-center items-center'>
+        <ul className='hidden lg:flex md:px-6 capitalize justify-center items-center'>
           {navLinks.map((link)=>{
             const {id, text, url}= link;
 
@@ -88,7 +88,6 @@ const Navbar = () => {
 
         {/* Sidebar menu */}
          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} navLink={navLink} />
-
         </nav>
     </header>
   )
