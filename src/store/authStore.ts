9c7db4921733dba52097032a830998dbdb63d0c4
rio-cabalda <riojoycabalda14@gmail.com>
2023-09-21 11:03:36 +0800

    Create authStore.ts

---
+++ src/store/authStore.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+
+type UserType = {
+    _id: string,
+    email: string,
+    firstname: string,
+    lastname: string,
+    cart: [],
+}
+
+export type AuthStateType = {
+    user: UserType | null;
+    accessToken: string | null;
+    login: (user: UserType, token: string) => void;
+    logout: () => void;
+    isAuthenticated: () => boolean;
+
+}
+
+const useAuthStore = create<AuthStateType>((set)=>({
+  user: null,
+  accessToken: null,
+  login: (user, accessToken) => set({ user, accessToken }),
+  logout: () => set({ user: null, accessToken: null }),
+  isAuthenticated: () => !!set().user && !!set().accessToken,
+}));
+
+export default useAuthStore;
