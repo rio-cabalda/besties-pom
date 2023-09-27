74f7e46a606abe37ab75abd72236dfa9e2dac9be
rio-cabalda <riojoycabalda14@gmail.com>
2023-09-27 18:51:43 +0800

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,8 @@
 import usePasswordToggle from './usePasswordToggle';
 import useConfirmPasswordToggle from './useConfirmPasswordToggle';
-import useFetchData from './useFetchProducts';
 import useFormatPrice from './useFormatPrice';
 import randomFeatured from './useFeaturedProducts';
 
 
 
-export { usePasswordToggle, useConfirmPasswordToggle, useFetchData, useFormatPrice, randomFeatured};
+export { usePasswordToggle, useConfirmPasswordToggle, useFormatPrice, randomFeatured};
