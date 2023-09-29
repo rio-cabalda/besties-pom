3ddcd540bbe18ea851ab2a6724e4a850533fe48c
rio-cabalda <riojoycabalda14@gmail.com>
2023-09-30 01:12:25 +0800

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,22 +1,27 @@
+import AddToCart from "./AddToCart";
 import CustomerRating from "./CustomerRating";
 import ErrorMessage from "./ErrorMessage";
 import FeaturedProducts from "./FeaturedProducts";
+import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import NavCart from "./NavCart";
 import Product from "./Product";
 import ProductList from "./ProductList";
+import RelatedProduct from "./RelatedProduct";
+import RelatedProducts from "./RelatedProducts";
+import SearchBar from "./SearchBar";
+import SectionTitle from "./SectionTitle";
+import Services from "./Services";
+import ServicesCard from "./ServicesCard";
 import Sidebar from "./Sidebar";
 import Sort from "./Sort";
-import Services from "./Services";
-import SectionTitle from "./SectionTitle";
-import Footer from "./Footer";
-import SearchBar from "./SearchBar";
-SearchBar
+
 
 export { CustomerRating,ErrorMessage,FeaturedProducts,Hero,Navbar,Product,
     ProductList,
     Sidebar,
     Sort,
-    Services,SectionTitle,Footer,SearchBar }
+    Services,SectionTitle,Footer,SearchBar,AddToCart,NavCart,RelatedProducts,ServicesCard,RelatedProduct  }
 
 
