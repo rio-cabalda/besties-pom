059fc0d9e55403840f776028c615a0093542caa3
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-04 19:55:49 +0800

    Update ProductsPage.tsx

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,8 +1,17 @@
-
+import {useEffect} from 'react';
 import {ProductList,Sort} from '../components';
 
 const ProductsPage = () => {
-    
+  
+  useEffect(()=>{
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth', // Smooth scrolling animation
+      });
+    };
+    scrollToTop();
+  },[]);
  
   return (
     <div className='max-w-screen-xl mx-auto flex flex-col justify-between items-center'>
