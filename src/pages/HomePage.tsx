81706db8a6a019352d22d0773b7a8db73dcf5c7c
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-04 19:55:47 +0800

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
 import {Hero, FeaturedProducts, Services} from '../components';
 
 const HomePage = () => {
-
+  
+  useEffect(()=>{
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth', // Smooth scrolling animation
+      });
+    };
+    scrollToTop();
+  },[]);
  
   return (
     <div className='max-w-screen-xl mx-auto flex flex-col'>
