4f8b78f4ad670f096c42cc06e0c9cfb37eb97b1c
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-04 20:41:46 +0800

    Update NavCart.tsx

--- src/components/NavCart.tsx
+++ src/components/NavCart.tsx
@@ -1,18 +1,34 @@
- import { useEffect } from 'react';
+ import { useEffect, useState } from 'react';
 import { BsBag } from 'react-icons/bs';
 import useAuthStore from '../store/authStore';
 import checkAuthUser from '../api/checkAuthUser';
 import { Link } from 'react-router-dom';
-
+import { AxiosError } from 'axios';
+import axiosPrivate from '../api/useAxiosConfig';
 
 const NavCart = () => {
-    const checkUser = checkAuthUser();
-    const {user, isAuthenticated} = useAuthStore();
+  const checkUser = checkAuthUser();
+  const {user, isAuthenticated, setlogoutUser} = useAuthStore();
+  const [userMenuOpen, setUserMenuOpen] = useState<boolean>(false);
+  const [loggingOut, setLoggingOut] = useState<boolean>(false);
 
-    useEffect(()=>{checkUser}
+  useEffect(()=>{checkUser}
    // eslint-disable-next-line
-,[]);
-console.log(user?.lastname);
+  ,[]);
+  
+  const logoutUser = async() =>{
+    setLoggingOut(true);
+    try {
+      await axiosPrivate.post('/user/logout');
+      setlogoutUser();
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      throw new Error(axiosError.message);
+    }finally{
+        setLoggingOut(false);
+        setUserMenuOpen(!userMenuOpen);
+    }
+}
 
   return (
     <div>
@@ -24,22 +40,31 @@ console.log(user?.lastname);
                   <span className='absolute top-0 left-1/2 -translate-x-1/2 translate-y-1/2 text-xs font-medium leading-0 leading-none flex items-center px-0.5'>10</span>
                 </Link>
               </div>
-              <div className=' w-10 h-10 rounded-full overflow-hidden cursor-pointer'>
-                  <img className='w-full h-full object-cover' src={user?.image} alt={user?.firstname} />  
+              <div className='w-10 h-10 rounded-full overflow-hidden cursor-pointer' onClick={()=>setUserMenuOpen(!userMenuOpen)}>
+                    <img className='w-full h-full object-cover' src={user?.image} alt={user?.firstname} />  
               </div>
+              
 
                {/* drop down user information */}
-              <div className='w-[15rem] custom-clip-path p-4 pt-16 absolute -bottom-0 translate-y-[100%] right-0 rounded-md flex flex-col items-center bg-blue-300'>
-                  <div className='w-16 h-16 rounded-full overflow-hidden cursor-pointer'>
-                      <img className='w-full h-full object-cover' src={user?.image} alt={user?.firstname} /> 
+               {userMenuOpen?
+              <div className={`w-[15rem] custom-clip-path p-4 pt-16 absolute -bottom-0 translate-y-[100%] right-0 rounded-md flex flex-col items-center bg-blue-300 text-slate-700 duration-300`}>
+                  <div className='w-[75px] h-[75px] rounded-full flex justify-center items-center bg-white'>
+                    <div className='w-16 h-16 rounded-full overflow-hidden cursor-pointer'>
+                        <img className='w-full h-full object-cover' src={user?.image} alt={user?.firstname} /> 
+                    </div>
                   </div>
-                  <h2 className='text-sm py-2 font-bold'>Hi, {user?.firstname}!</h2> 
+                  <h2 className='text-base py-2 font-bold '>Hi, {user?.firstname}!</h2> 
                   <span className='text-sm rounded-full p-4 py-1 border-2 border-slate-600'>{user?.email}</span> 
-                  <button type='button' className='capitalize mt-5 px-2 py-1 bg-blue-700 rounded-md'>logout</button>
-                <div>
-                  
-                </div>
-                </div>
+                  <button type='button' className='w-24 h-8 flex justify-center items-center capitalize mt-5 bg-blue-700 text-slate-200 rounded-md' onClick={logoutUser}>
+                    {loggingOut? 
+                    <div className="inline-block ml-2 h-5 w-5 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-slate-500 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                    role="status">
+                        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+                    </div>
+                    :
+                    'logout'}
+                    </button>
+                </div> : null}
             </div>
             :
             <div className='hidden md:inline-block'>
