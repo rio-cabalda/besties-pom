ddbd641b49e606bb56fd683625f94916ab9bdbf7
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-08 09:38:05 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import {SignUp, SignIn} from "./pages/auth";
-import {Navbar,Footer} from "./components";
+import { SignUp, SignIn } from "./pages/auth";
+import { Navbar } from "./components";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home,Products, Error, SingleProduct } from "./pages";
 import { useProductStore } from "./store/productStore";
-import Checkout from "./pages/Checkout";
+import ShoppingCart from "./pages/ShoppingCart";
 import PrivateRoute from "./pages/PrivateRoute";
 
 function App() {
@@ -13,25 +13,27 @@ function App() {
   };
   
   return (
-    <BrowserRouter>
-      <Navbar/>
-        <main style={divStyle}>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path='signup' element={<SignUp/>} />
-            <Route path='signin' element={<SignIn/>} />
-            <Route path='products' element={<Products/>} />
-            <Route path='products/:id' element={<SingleProduct />} />
-            <Route path="checkout" element={
-                      <PrivateRoute >
-                            <Checkout />
-                      </PrivateRoute>
-            } />
-            <Route path='*' element={<Error />} />
-          </Routes>
-        </main>
-      <Footer />
-    </BrowserRouter>
+    <div className="max-w-screen-xl mx-auto">
+      <BrowserRouter>
+        <Navbar/>
+          <main style={divStyle}>
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path='signup' element={<SignUp/>} />
+              <Route path='signin' element={<SignIn/>} />
+              <Route path='products' element={<Products/>} />
+              <Route path='products/:id' element={<SingleProduct />} />
+              <Route path="cart/:id" element={
+                        <PrivateRoute >
+                              <ShoppingCart />
+                        </PrivateRoute>
+              } />
+              <Route path='*' element={<Error />} />
+            </Routes>
+          </main>
+          {/* <Footer /> */}
+        </BrowserRouter>
+    </div>
   )
 }
 
