06751d7449624fc3434f53649cc41ccffde2e70d
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-10 20:27:56 +0800

    Update index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,8 @@ import Home from './HomePage';
 import Products from './ProductsPage';
 import Error from './ErrorPage';
 import SingleProduct from './SingleProductPage';
+import PrivateRoute from './PrivateRoute';
+import About from './AboutPage';
+import ShoppingCart from './ShoppingCartPage';
 
-export {Home, Products, Error, SingleProduct}
+export {Home, Products, Error, SingleProduct, PrivateRoute, About,ShoppingCart}
