c2f6ff0cdf0dc645720823af755c3c717cb78a7f
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-10 20:27:32 +0800

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
 import AddToCart from "./AddToCart";
+import CartCheckout from "./CartCheckout";
+import CartItemQuantity from "./CartItemQuantity";
+import CartItems from "./CartItems";
+import CustomerComment from "./CustomerComment";
 import CustomerRating from "./CustomerRating";
 import ErrorMessage from "./ErrorMessage";
 import FeaturedProducts from "./FeaturedProducts";
@@ -6,6 +10,7 @@ import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import NavCart from "./NavCart";
+import PaypalBtn from "./PaypalBtn";
 import Product from "./Product";
 import ProductList from "./ProductList";
 import RelatedProduct from "./RelatedProduct";
@@ -16,12 +21,12 @@ import Services from "./Services";
 import ServicesCard from "./ServicesCard";
 import Sidebar from "./Sidebar";
 import Sort from "./Sort";
-
+import StarRating from "./StarRating";
 
 export { CustomerRating,ErrorMessage,FeaturedProducts,Hero,Navbar,Product,
     ProductList,
     Sidebar,
     Sort,
-    Services,SectionTitle,Footer,SearchBar,AddToCart,NavCart,RelatedProducts,ServicesCard,RelatedProduct  }
+    Services,SectionTitle,Footer,SearchBar,AddToCart,NavCart,RelatedProducts,ServicesCard,RelatedProduct,CartItems,CartItemQuantity,StarRating,CustomerComment,PaypalBtn,CartCheckout  }
 
 
