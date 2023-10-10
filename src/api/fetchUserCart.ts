a1c357242140f25afd035e8a8e48602c756b9005
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-10 20:27:13 +0800

    Update fetchUserCart.ts

--- src/api/fetchUserCart.ts
+++ src/api/fetchUserCart.ts
@@ -1,27 +1,31 @@
-import { useQuery } from "react-query";
+import { useQuery, QueryObserverResult  } from "react-query";
 import axiosPrivate from "./axiosConfig";
 import { AxiosError } from "axios";
+import { CartItemType } from "../pages/ShoppingCartPage";
 
-const useUserCart = (id:string | undefined) => {
-    const { data:cartItems, isLoading, isError } = useQuery(['userCart',id], async()=>{
+type UseUserCartType = {
+    cartItems: CartItemType[];
+    isLoading: boolean;
+    isError: boolean;
+    refetch: () => Promise<QueryObserverResult<CartItemType[], unknown>>
+}
+
+const useUserCart = (id:string | undefined):UseUserCartType  => {
+    const { data:cartItems, isLoading, isError, refetch } = useQuery(['userCart',id], async()=>{
         try {
             const response = await axiosPrivate.get('/user/cart');
             const cartItems = response.data;
-
-            if(cartItems){
-                return cartItems;
-            }
-            else{
-                return [];
-            }
+            return cartItems || []; 
         } catch (error) {
             const axiosError = error as AxiosError;
             console.log('error inside useQuery');
             
             throw new Error(axiosError.message)
         }
+    }, {
+        refetchOnWindowFocus: false, // Disable automatic refetch on window focus
     });
-    return { cartItems, isLoading, isError }
+    return { cartItems, isLoading, isError, refetch}
 }
 
 export default useUserCart;
