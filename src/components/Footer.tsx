371a4ae61761e0f7efce05ddd2422724e82a9e04
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-12 09:49:16 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-   <footer className="bg-gray-800 text-white p-4">
+   <footer className="flex items-center bg-gray-800 text-white p-4 h-16">
     <div className="container mx-auto text-center">
       <p>&copy; 2023 Besties Pom Petshop. All rights reserved.</p>
     </div>
